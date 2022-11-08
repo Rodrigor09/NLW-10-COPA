33d1a5107a99e16a0a2a5ff0dd677f4cd47bc802
Rodrigor09 <117116591+Rodrigor09@users.noreply.github.com>
2022-11-08 19:23:45 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -22,9 +22,31 @@ function createCard(date, day, games) {
 }
 
 document.querySelector("#cards").innerHTML =
-        createCard("24/11", "quinta",createGame("brazil", "16:00", "serbia"))
+        createCard("20/11", "domingo",createGame("qatar", "13:00", "ecuador"))
+        +
+        createCard("21/11", "segunda",createGame("senegal", "13:00", "netherlands"))  
+        +
+        createCard("22/11", "terça",createGame("argentina", "07:00", "arabia") + 
+        createGame("france", "16:00", "australia")) 
+        +
+        createCard("23/11", "quarta",createGame("germany", "10:00", "japan") +
+        createGame("spain", "13:00", "costarica") + createGame("belgium", "16:00", "canada"))  
+        +       
+        createCard("24/11", "quinta",createGame("portugal", "13:00", "ghana") + createGame("brazil", "16:00", "serbia"))        
+        +
+        createCard("25/11", "sexta",createGame("netherlands", "13:00", "ecuador"))
+        +
+        createCard("26/11", "sabado",createGame("france", "13:00", "denmark") + 
+        createGame("argentina", "16:00", "mexico"))
+        +
+        createCard("27/11", "domingo",createGame("spain", "16:00", "germany"))
         +
         createCard("28/11", "segunda",createGame("switzerland", "13:00", "brazil") + 
         createGame("portugal", "16:00", "uruguay"))
         +
-        createCard("02/12","sexta",createGame("brazil", "16:00", "cameroon"))
+        createCard("30/11", "quarta",createGame("poland", "16:00", "argentina"))
+        +
+        createCard("01/12","quinta",createGame("croatia", "12:00", "belgium"))
+        +
+        createCard("02/12","sexta",createGame("korea", "12:00", "portugal") +
+        createGame("brazil", "16:00", "cameroon"))
